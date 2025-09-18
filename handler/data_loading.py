bbd978fba4598befaa1c65c79956e1d3c1df2151
Nayan <nayan.mane@emerson.com>
2025-09-18 19:48:25 +0530

    Created customer churn prediction poc

--- handler/data_loading.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(filepath):
-    df = pd.read_csv(filepath)
-    return df
-
-if __name__ == "__main__":
-    df = load_data('data/Vodafone_Customer_Churn_Sample_Dataset.csv')
-    print(df.head())
